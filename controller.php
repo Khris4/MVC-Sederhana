f0d0dd0cc67c2bca542794a05f234e21d2740883
Khris4 <90852723+Khris4@users.noreply.github.com>
2022-12-17 14:46:11 +0700

    MVC Sederhana

---
+++ controller.php
@@ -0,0 +1,12 @@
+<?php
+class Controller {
+ 
+public function __construct() {
+$model = new Model;
+$data = $model->show_message();
+ 
+require_once 'view.php';
+}
+ 
+}
+?>
---
+++ model.php
@@ -0,0 +1,13 @@
+<?php
+
+class Model {
+ 
+    public function show_message() {
+    return array(
+    'title' => 'MVC Sederhana',
+    'body'  => 'Hello, World! menggunakan konsep MVC'
+    );
+    }
+     
+}
+?>
